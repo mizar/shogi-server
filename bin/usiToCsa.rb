e37932a0d2387227928808ec86f49362ae29f9ea
Mizar <mizar.jp@gmail.com>
2020-09-25 23:39:17 +0900

    tcp_keepalive

--- bin/usiToCsa.rb
+++ bin/usiToCsa.rb
@@ -28,6 +28,7 @@ $:.unshift(File.join(File.dirname(File.expand_path(__FILE__)), ".."))
 require 'shogi_server'
 require 'logger'
 require 'socket'
+require 'timeout'
 
 # Global variables
 
@@ -105,7 +106,12 @@ def parse_command_line
     ["--options",     GetoptLong::REQUIRED_ARGUMENT],
     ["--password",    GetoptLong::REQUIRED_ARGUMENT],
     ["--ponder",      GetoptLong::NO_ARGUMENT],
-    ["--port",        GetoptLong::REQUIRED_ARGUMENT])
+    ["--port",        GetoptLong::REQUIRED_ARGUMENT],
+    ["--ignoremultiselect",  GetoptLong::NO_ARGUMENT],
+    ["--tcpkeepalive",       GetoptLong::REQUIRED_ARGUMENT],
+    ["--tcpkeepalive-idle",  GetoptLong::REQUIRED_ARGUMENT],
+    ["--tcpkeepalive-intvl", GetoptLong::REQUIRED_ARGUMENT],
+    ["--tcpkeepalive-cnt",   GetoptLong::REQUIRED_ARGUMENT])
   parser.quiet = true
   begin
     parser.each_option do |name, arg|
@@ -132,6 +138,15 @@ def parse_command_line
   options[:ponder]      ||= ENV["PONDER"] || false
   options[:port]        ||= ENV["PORT"] || 4081
   options[:port]        = options[:port].to_i
+  options[:ignoremultiselect]  ||= ENV["IGNORE_MULTISELECT"] || false
+  options[:tcpkeepalive]       ||= ENV["TCPKEEPALIVE"] || 0
+  options[:tcpkeepalive]       = options[:tcpkeepalive].to_i
+  options[:tcpkeepalive_idle]  ||= ENV["TCPKEEPALIVE_IDLE"] || 50
+  options[:tcpkeepalive_idle]  = options[:tcpkeepalive_idle].to_i
+  options[:tcpkeepalive_intvl] ||= ENV["TCPKEEPALIVE_INTVL"] || 5
+  options[:tcpkeepalive_intvl] = options[:tcpkeepalive_intvl].to_i
+  options[:tcpkeepalive_cnt]   ||= ENV["TCPKEEPALIVE_CNT"] || 9
+  options[:tcpkeepalive_cnt]   = options[:tcpkeepalive_cnt].to_i
 
   return options
 end
@@ -424,7 +439,14 @@ class BridgeState
       throw "Bad state at event_engine_recv: #@state"
     end
 
-    str = $server.gets
+    str = nil
+    begin
+      Timeout.timeout(0.001){
+        str = $server.gets
+      }
+    rescue Timeout::Error
+      $logger.error "event_server_recv timeout"
+    end
     return if str.nil? || str.strip.empty?
     log_server_recv str
 
@@ -567,6 +589,30 @@ def login
   begin
     $server = TCPSocket.open($options[:host], $options[:port])
     $server.sync = true
+    if $options[:tcpkeepalive] > 0 &&
+      Socket.const_defined?(:SOL_SOCKET) &&
+      Socket.const_defined?(:SO_KEEPALIVE) &&
+      Socket.const_defined?(:IPPROTO_TCP)
+      $server.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, true)
+      if Socket.const_defined?(:TCP_KEEPIDLE) &&
+        Socket.const_defined?(:TCP_KEEPINTVL) &&
+        Socket.const_defined?(:TCP_KEEPCNT)
+        $server.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPIDLE, $options[:tcpkeepalive_idle])
+        $server.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPINTVL, $options[:tcpkeepalive_intvl])
+        $server.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_KEEPCNT, $options[:tcpkeepalive_cnt])
+      elsif RUBY_PLATFORM.downcase =~ /mswin|mingw|cygwin|bccwin/
+        # Windows 10 (1709 or later) ws2ipdef.h
+        # https://docs.microsoft.com/en-us/windows/win32/winsock/ipproto-tcp-socket-options
+        $server.setsockopt(Socket::IPPROTO_TCP, 3, $options[:tcpkeepalive_idle])
+        $server.setsockopt(Socket::IPPROTO_TCP, 17, $options[:tcpkeepalive_intvl])
+        $server.setsockopt(Socket::IPPROTO_TCP, 16, $options[:tcpkeepalive_cnt])
+      elsif RUBY_PLATFORM.downcase =~ /darwin/
+        # macOS 10.12.6 (Sierra) /usr/include/netinet/tcp.h
+        $server.setsockopt(Socket::IPPROTO_TCP, 0x10, $options[:tcpkeepalive_idle])
+        $server.setsockopt(Socket::IPPROTO_TCP, 0x101, $options[:tcpkeepalive_intvl])
+        $server.setsockopt(Socket::IPPROTO_TCP, 0x102, $options[:tcpkeepalive_cnt])
+      end
+    end
   rescue
     log_error "Failed to connect to the server"
     $server = nil
@@ -611,7 +657,7 @@ end
 #
 def main_loop
   while true
-    ret, = select([$server, $engine], nil, nil, 60)
+    ret, = select([$engine, $server], nil, nil, 60)
     unless ret
       # Send keep-alive
       if $bridge_state.too_quiet?
@@ -621,12 +667,14 @@ def main_loop
       next
     end
 
-    ret.each do |io|
-      case io
-      when $engine
-        $bridge_state.do_engine_recv
-      when $server
-        $bridge_state.do_sever_recv
+    ret.each_with_index do |io, idx|
+      if (!$options[:ignoremultiselect] || idx == 0)
+        case io
+        when $engine
+          $bridge_state.do_engine_recv
+        when $server
+          $bridge_state.do_sever_recv
+        end
       end
     end
 
